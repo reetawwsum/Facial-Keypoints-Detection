a5dc85a49093a70a5a8d97b2e63e214c4ebf2037
Reet Awwsum <reetawwsum@yahoo.com>
2016-07-01 11:24:45 +0530

    Reading the training and test file

--- facial_keypoints_detection_input.py
+++ facial_keypoints_detection_input.py
@@ -4,23 +4,63 @@ import matplotlib.pyplot as plt
 
 file_path = 'dataset/'
 train_file = 'training.csv'
+test_file = 'test.csv'
 
-with open(file_path + train_file, 'rb') as f:
-	csv_reader = csv.reader(f, delimiter=',')
+def read_train_file(max_images=None):
+	with open(file_path + train_file, 'rb') as f:
+		csv_reader = csv.reader(f, delimiter=',')
 
-	csv_reader.next()
-	targets = []
-	images = []
+		csv_reader.next()
+		targets = []
+		images = []
 
-	for row in csv_reader:
-		targets.append(row[:-1])
-		images.append(row[-1].split())
+		for row in csv_reader:
+			if csv_reader.line_num == max_images:
+				break
 
-images = np.array(images).astype(int)
+			targets.append(row[:-1])
+			images.append(row[-1].split())
 
-image = images[0]
-target = targets[0]
+		images = np.array(images).astype(int)
 
-plt.imshow(np.reshape(images[0], (96, 96)), cmap='gray')
-plt.plot(target[0::2], target[1::2], 'o')
-plt.show()
+	return images, targets
+
+def read_test_file(max_images=None):
+	with open(file_path + test_file, 'rb') as f:
+		csv_reader = csv.reader(f, delimiter=',')
+
+		csv_reader.next()
+		images = []
+
+		for row in csv_reader:
+			if csv_reader.line_num == max_images:
+				break
+
+			images.append(row[-1].split())
+		
+		images = np.array(images).astype(int)
+
+	return images
+
+
+def plot_image(image, target=None):
+	plt.figure()
+
+	plt.imshow(np.reshape(image, (96, 96)), cmap='gray')
+
+	if target is not None:
+		plt.plot(target[0::2], target[1::2], 'o')
+
+	return plt
+
+if __name__ == '__main__':
+	train_images, targets = read_train_file(10)
+	test_images = read_test_file(10)
+
+	random_train_index = np.random.randint(train_images.shape[0])
+	random_test_index = np.random.randint(test_images.shape[0])
+
+	plot_image(train_images[random_train_index], targets[random_train_index])
+	plot_image(test_images[random_test_index])
+
+	plt.show()
