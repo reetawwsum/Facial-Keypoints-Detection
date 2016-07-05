65d60b3fcb515c38bf0c85411d4028e782b88dac
Reet Awwsum <reetawwsum@yahoo.com>
2016-07-05 21:59:14 +0530

    Adding dropout layer and increasing the units in fully connected layer

--- facial_keypoints_detection_model.py
+++ facial_keypoints_detection_model.py
@@ -33,42 +33,50 @@ def inference(images):
 
 	pool1 = max_pool_2x2(conv1)
 
+	conv1_drop = tf.nn.dropout(pool1, 0.1)
+
 	# Convolutional layer 2
 	with tf.name_scope('conv2'):
 		weights = weight_variable([2, 2, 32, 64])
 		biases = bias_variable([64])
 
-		conv2 = tf.nn.relu(conv2d(pool1, weights) + biases)
+		conv2 = tf.nn.relu(conv2d(conv1_drop, weights) + biases)
 
 	pool2 = max_pool_2x2(conv2)
+
+	conv2_drop = tf.nn.dropout(pool2, 0.2)
 
 	# Convolutional layer 3
 	with tf.name_scope('conv3'):
 		weights = weight_variable([2, 2, 64, 128])
 		biases = bias_variable([128])
 
-		conv3 = tf.nn.relu(conv2d(pool2, weights) + biases)
+		conv3 = tf.nn.relu(conv2d(conv2_drop, weights) + biases)
 
 	pool3 = max_pool_2x2(conv3)
 
+	conv3_drop = tf.nn.dropout(pool3, 0.3)
+
 	# Fully connected layer 1
 	with tf.name_scope('fc1'):
-		weights = weight_variable([12 * 12 * 128, 500])
-		biases = bias_variable([500])
+		weights = weight_variable([12 * 12 * 128, 1000])
+		biases = bias_variable([1000])
 
-		pool3_flat = tf.reshape(pool3, [-1, 12 * 12 * 128])
+		pool3_flat = tf.reshape(conv3_drop, [-1, 12 * 12 * 128])
 		fc1 = tf.nn.relu(tf.matmul(pool3_flat, weights) + biases)
+
+	fc1_drop = tf.nn.dropout(fc1, 0.5)
 
 	# Fully connected layer 2
 	with tf.name_scope('fc2'):
-		weights = weight_variable([500, 500])
-		biases = bias_variable([500])
+		weights = weight_variable([1000, 1000])
+		biases = bias_variable([1000])
 
-		fc2 = tf.nn.relu(tf.matmul(fc1, weights) + biases)
+		fc2 = tf.nn.relu(tf.matmul(fc1_drop, weights) + biases)
 
 	# Linear layer
 	with tf.name_scope('linear'):
-		weights = weight_variable([500, 30])
+		weights = weight_variable([1000, 30])
 		biases = bias_variable([30])
 
 		logits = tf.matmul(fc2, weights) + biases
